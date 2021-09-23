29cfae97a48f86eedf6838260ee7b01165680b1e
alexju <alexju@tencent.com>
2021-09-23 19:24:10 +0800

    set up project

---
+++ README.md
@@ -0,0 +1,5 @@
+# WebRTC NS
+
+This repo is the separation part of WebRTC noise suppresion module.
+
+It is recommended that you add it to your project as a static library.
--- WebRTCNS/WebRTCNS.h
+++ WebRTCNS/WebRTCNS.h
@@ -5,14 +5,4 @@
 //  Created by alexju on 2021/9/23.
 //
 
-#import <Foundation/Foundation.h>
-
-//! Project version number for WebRTCNS.
-FOUNDATION_EXPORT double WebRTCNSVersionNumber;
-
-//! Project version string for WebRTCNS.
-FOUNDATION_EXPORT const unsigned char WebRTCNSVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <WebRTCNS/PublicHeader.h>
-
-
+#import "noise_suppression.h"
