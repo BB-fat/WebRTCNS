3214cc7e830f41f64a6509317683c43ce87dcd7a
alexju <alexju@tencent.com>
2021-09-23 19:18:39 +0800

    Initial Commit

---
+++ WebRTCNS/WebRTCNS.h
@@ -0,0 +1,18 @@
+//
+//  WebRTCNS.h
+//  WebRTCNS
+//
+//  Created by alexju on 2021/9/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebRTCNS.
+FOUNDATION_EXPORT double WebRTCNSVersionNumber;
+
+//! Project version string for WebRTCNS.
+FOUNDATION_EXPORT const unsigned char WebRTCNSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebRTCNS/PublicHeader.h>
+
+
